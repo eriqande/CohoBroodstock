e25fec8a1ae8dd7839a69e55a4e99b7901714b9e
Eric C. Anderson <eric.anderson@noaa.gov>
2019-11-05 12:48:52 -0700

    Remove loci that are all missing data.
    This keeps related from choking.

--- R/computeRxy.R
+++ R/computeRxy.R
@@ -12,6 +12,13 @@
 #'                     "WSH_W1718_v5_two_column_data.txt.gz",
 #'                     package = "CohoBroodstock")
 #' rxy <- computeRxy(file)
+#'
+#' # here is one that uses a file that has some loci that must be dropped
+#' file2 <- system.file("extdata",
+#'                     "IGH_W1920_v1_norefs.txt.gz",
+#'                     package = "CohoBroodstock")
+#' rxy <- computeRxy(file2)
+
 computeRxy <- function(file) {
   # read in the lines and then print them back after removing the first
   X <- read.table(file, stringsAsFactors = FALSE, header = TRUE)
@@ -19,6 +26,23 @@ computeRxy <- function(file) {
   # if any columns are all NA (happens with microsoft excel-saved files, I think)
   # then remove them.
   X <- X[!sapply(X, function(x) all(is.na(x)))]
+
+  # now, it turns out that any loci that have nothing by missing data at them
+  # must be removed, or related will bomb on it.  So, we do some quick machinations to
+  # find those.  Recall that 0 means missing data.
+  # first, get the data in single column format
+  xm <- matrix(as.matrix(X[,-1]), ncol = (ncol(X) - 1) / 2)
+  # then figure out which loci have only a single observed allele
+  bad_hombres <- which(apply(xm, 2, function(col) {
+    all(col == 0)
+  }))
+  if(length(bad_hombres) > 0) {
+    # figure out which indexes in X to drop:
+    drop_these <- rep(2 * bad_hombres, each = 2) + c(0, 1)
+    X <- X[, -drop_these]
+  }
+
+
 
   tmpf <- tempfile()
   readr::write_tsv(X, tmpf, col_names = FALSE)
@@ -34,6 +58,13 @@ computeRxy <- function(file) {
   # then put those names back in there:
   nn1 <- new_names %>% dplyr::rename(ind1 = real_name, ind1.id = simple_name)
   nn2 <- new_names %>% dplyr::rename(ind2 = real_name, ind2.id = simple_name)
+
+
+  # print a message if loci were dropped
+  # and print a message
+  if(length(bad_hombres) > 0) {
+    message("Dropped these loci b/c they were all missing data: ", paste(names(X)[bad_hombres * 2], collapse = ", "), ".")
+  }
 
   ret <- tibble::as_tibble(rel$relatedness) %>%
     dplyr::select(ind1.id, ind2.id, quellergt) %>%
